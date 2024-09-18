0a3b216ea18559ee26401449c70f42e165ce53e0
mdkubiak <mark.kubiak@kiongroup.com>
2024-09-18 21:28:12 +0000

    terraform and azure variables

--- azure-tf/main.tf
+++ azure-tf/main.tf
@@ -15,7 +15,36 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "rg" {
-  name     = "myTFResourceGroup"
-  location = "westus2"
+  name     = var.name
+  location = var.loc
 }
 
+resource "azurerm_virtual_network" "myvirtualnetwork" {
+  name                = var.virtual_network_name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  address_space       = var.address_space
+
+}
+
+
+variable "loc" {
+  type    = string
+  default = "westus2"
+}
+
+variable "name" {
+  type    = string
+  default = "myTFResourceGroup"
+}
+
+variable "virtual_network_name" {
+  type    = string
+  default = "myvirtualnetwork"
+}
+
+variable "address_space" {
+  type = list(string)
+  default = ["10.0.0.0/16"]
+
+}
